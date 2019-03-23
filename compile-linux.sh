8aaa8a79234246c6497f2436f57a5df103f60ba5
Ravbug <ravbug@github.com>
2019-03-23 14:07:29 -0700

    Added flag (-r) to run after successful compile

--- compile-linux.sh
+++ compile-linux.sh
@@ -1,5 +1,26 @@
+root="wxWidgets/";
+path="build/linux/app/";
+name="wxTemplateLinux";
+
+mkdir -p $root$path;
+
 # Compile dynamic linking
-g++ `wx-config --cppflags` `wx-config --libs` -Wl,-rpath,/usr/local/lib/ -o wxTemplate source/main.cpp
+g++ `wx-config --cppflags` `wx-config --libs` -Wl,-rpath,/usr/local/lib/ -o $root$path$name source/main.cpp &&
+
+# print success
+
+echo "    Build succeeded. Wrote executable to" $root$path$name;
+
+# launch application if flag present
+
+while getopts ":r" OptionArgument; do
+case $OptionArgument in
+	
+	r ) echo "Launching $name"; ./$root$path$name ;;
+	* ) echo "Unknown argument passed. Use -r to launch the program after successful compile";;
+esac
+done
+
 
 # Compile static linking
 #g++ `wx-config --static --cppflags` `wx-config --static --libs` -I wxWidgets/include/ -I wxWidgets/build/linux/lib/wx/include/gtk3-unicode-3.1 -o wxTemplate source/main.cpp
