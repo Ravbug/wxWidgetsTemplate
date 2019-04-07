090782e4dc6aa57727f02f59592641aff2a5ef21
Ravbug <ravbug@localhost.localdomain>
2019-04-07 00:21:27 -0700

    + Added better error handling around potential failure points

--- setup-linux.sh
+++ setup-linux.sh
@@ -7,29 +7,43 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" ;
 cd $DIR;
 
 # make the build directory
-mkdir wxWidgets/build/linux;
+mkdir -p wxWidgets/build/linux;
 cd wxWidgets/build/linux;
 
 # run the configure script
-../../configure --enable-unicode --enable-debug &&
+../../configure --enable-unicode --enable-debug && success=1
 
-threads=$(nproc);
+if [ $success = 1 ]; then
+	threads=$(nproc);
 
-echo "Starting silent make on $threads processes...";
+	echo "Starting silent make on $threads processes...";
 
-# build the library (only print errors)
-make -j$threads --silent &&
+	# build the library (only print errors)
+	make -j$threads --silent && ms=1
 
-# install (requires priviledges)
-echo "Starting the silent make install, please enter the administrator password if prompted"
-sudo make install --silent
+	if [ $ms = 1 ]; then
+		# install (requires priviledges)
+		echo "Starting the silent make install, please enter the administrator password if prompted"
+		sudo make install --silent && mi=1
+		
+		if [ $mi=1 ]; then
+			# test the installed library
+			echo "wx-config: Currently installed wxWidgets version is $(wx-config --version)";
 
-# test the installed library
-echo "wx-config: Currently installed wxWidgets version is $(wx-config --version)";
+			#echo "Running ldconfig to configure the dynamic linker runtime bindings, please enter the administrator password if prompted."
+			#sudo /sbin/ldconfig /usr/local/lib;
 
-#echo "Running ldconfig to configure the dynamic linker runtime bindings, please enter the administrator password if prompted."
-#sudo /sbin/ldconfig /usr/local/lib;
+			echo "Setup is complete!"
+		else
+			echo "Make install failed with errors. Please check the above output, correct the errors, and re-run this script."
+		fi
+	else
+		echo "Make failed with errors. Please check the above output, correct the errors, and re-run this script."
+	fi
+	
+else
+	echo "Configure failed with errors. Please check the above output, correct the errors, and re-run this script."
+fi
 
-echo "Setup is complete!"
 # set the directory back to the original script directory
 cd $DIR;
