15f7ee3e6217a1710fc3026df157c0a83a8b1430
Ravbug <ravbug@localhost.localdomain>
2019-05-03 13:36:38 -0700

    Refactored
    + Added functions
    
    + Added minimal colors to make output easier to read

--- setup-linux.sh
+++ setup-linux.sh
@@ -1,58 +1,78 @@
 # wxWidgets Linux setup script by Ravbug (github.com/ravbug)
 # This script will set up wxWidgets for use on your Linux computer. 
-# To prevent Make from flooding the shell, I redirect the non-error output to /dev/null. Feel free to disable this if you have issues.
+
+# function that prints an error message and stops the script
+# $1 = what failed
+exitWithMessage(){
+	echo -e "\n\e[101m$1\e[49m\e[91m failed with errors. Check the above output, correct the errors, and re-run this script.\e[39m";
+	exit 1;
+}
+
+echoStatus(){
+	echo -e "\e[45m$1:\e[49m $2"
+}
+
 
 # get the script directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" ;
 cd $DIR;
 
+# run autogen.sh
 cd wxWidgets/
-echo "Running autogen.sh"
-./autogen.sh && ags=1
+echoStatus "autogen" "Running autogen.sh"
+success=0;
+./autogen.sh && success=1;
 
-if [ $ags = 1 ]; then
-	# make the build directory
-	mkdir -p build/linux;
-	cd build/linux;
-
-	echo "Running configure script"
-	# run the configure script
-	../../configure --enable-unicode --enable-debug && success=1
-
-	if [ $success = 1 ]; then
-		threads=$(nproc);
-
-		echo "Starting silent make on $threads processes...";
-
-		# build the library (only print errors)
-		make -j$threads --silent && ms=1
-
-		if [ $ms = 1 ]; then
-			# install (requires priviledges)
-			echo "Starting the silent make install, please enter the administrator password if prompted"
-			sudo make install --silent && mi=1
-			
-			if [ $mi=1 ]; then
-				# test the installed library
-				echo "wx-config: Currently installed wxWidgets version is $(wx-config --version)";
-
-				#echo "Running ldconfig to configure the dynamic linker runtime bindings, please enter the administrator password if prompted."
-				#sudo /sbin/ldconfig /usr/local/lib;
-
-				echo "Setup is complete!"
-			else
-				echo "Make install failed with errors. Check the above output, correct the errors, and re-run this script."
-			fi
-		else
-			echo "Make failed with errors. Check the above output, correct the errors, and re-run this script."
-		fi
-		
-	else
-		echo "Configure failed with errors. Check the above output, correct the errors, and re-run this script."
-	fi
-else
-	echo "autogen.sh failed with errors. Check the above output, correct the errors, and re-run this script.";
+# check success
+if [ $success = 0 ]; then
+	exitWithMessage "autogen.sh"
 fi
+
+# run configure
+# make the build directory
+mkdir -p build/linux;
+cd build/linux;
+
+echoStatus "configure" "Running configure script"
+# run the configure script
+success=0;
+../../configure --enable-unicode --enable-debug && success=1
+
+# check success
+if [ $success = 0 ]; then
+	exitWithMessage "configure"
+fi
+
+# start make
+threads=$(nproc);
+
+echoStatus "make" "Starting silent make on $threads processes...";
+
+# build the library (only print errors)
+success=0
+make -j$threads --silent && success=1
+
+# check success
+if [ $success = 0 ]; then
+	exitWithMessage "make"
+fi
+
+# install (requires priviledges)
+echoStatus "make install" "Starting the silent make install, enter the administrator password if prompted"
+success=0
+sudo make install --silent && success=1
+
+if [ $success = 0 ]; then
+	exitWithMessage "make install"
+fi
+
+# test the installed library
+echo -e "\e[45mwx-config:\e[49m Currently installed wxWidgets version is \e[44m$(wx-config --version)\e[49m";
+
+#echo "Running ldconfig to configure the dynamic linker runtime bindings, please enter the administrator password if prompted."
+#sudo /sbin/ldconfig /usr/local/lib;
+
+echo -e "\e[92msetup-linux is complete!\e[39m"
 
 # set the directory back to the original script directory
 cd $DIR;
